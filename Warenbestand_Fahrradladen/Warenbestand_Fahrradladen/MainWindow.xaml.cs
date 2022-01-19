bd2e0a236890292ed1a513b159df4464ab7a468a
dcrnv <92800083+dcrnv@users.noreply.github.com>
2022-01-19 17:47:48 +0100

    Warenbestand - WPF Programm

---
+++ Warenbestand_Fahrradladen/Warenbestand_Fahrradladen/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Warenbestand_Fahrradladen
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }      
+
+  
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Es hat geklappt");
+        }
+
+        private void Button_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                MessageBox.Show("Es hat geklappt");
+            }
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                MessageBox.Show("Es hat geklappt");
+            }
+
+        }
+    }
+}
