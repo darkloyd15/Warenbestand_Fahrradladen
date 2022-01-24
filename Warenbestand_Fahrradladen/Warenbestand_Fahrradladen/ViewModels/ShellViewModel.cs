4ffbdbbfdae2966bd64c9dc45616debc3ab7b113
Drilon Cernoveri <d.crnv@outlook.de>
2022-01-24 16:37:46 +0100

    Update Oberflächenprorgramm except: Verkaufsseite

---
+++ Warenbestand_Fahrradladen/Warenbestand_Fahrradladen/ViewModels/ShellViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warenbestand_Fahrradladen.ViewModels
+{
+    class ShellViewModel : Conductor<object>
+    {
+        public ShellViewModel()
+        {
+            ActivateItemAsync(IoC.Get<LoginViewModel>());
+        }
+    }
+}
